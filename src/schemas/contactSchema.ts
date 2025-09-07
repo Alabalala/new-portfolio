ff5acf517e37434962c9499dcbce82afe56c3fed
Alabalala <allareiksa@gmail.com>
2025-09-07 14:07:57 +0200

    changed error logic so messages are shown below corresponding input. Also changed input style to reflect error

--- src/schemas/contactSchema.ts
+++ src/schemas/contactSchema.ts
@@ -1,7 +1,13 @@
 import { z } from "zod";
 
 export const contactSchema = z.object({
-    name: z.string().trim().min(3, "❗Name is required"),
+    name: z.string()
+    .trim()
+    .min(1, "❗Name shouldn't be empty")
+    .min(3, "❗Name should be longer than 3 characters."),
     email: z.string().email("❗Invalidad email address"),
-    message: z.string().trim().min(10, "❗Message must be longer than 10 characters"),
+    message: z.string()
+    .trim()
+    .min(1, "❗Message shouldn't be empty")
+    .min(10, "❗Message must be longer than 10 characters"),
   });
