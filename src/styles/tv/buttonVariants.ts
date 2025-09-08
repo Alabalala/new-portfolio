0cc2eeec19198c73056218be582c73273efe785b
Alabalala <allareiksa@gmail.com>
2025-09-08 13:26:35 +0200

    added button hover and active effects

--- src/styles/tv/buttonVariants.ts
+++ src/styles/tv/buttonVariants.ts
@@ -1,15 +1,14 @@
 import { tv } from "tailwind-variants";
 
 export const buttonVariants = tv({
-    base: "px-4 transition-colors font-bebas text-2xl text-center md:text-4xl lg:text-5xl md:px-8 md:py-2",
+    base: "px-4 transition-colors font-bebas text-2xl text-center md:text-4xl lg:text-5xl md:px-8 md:py-2 cursor-pointer active:shadow-none active:scale-95 transition-transform transition-shadow duration-300",
     variants: {
         buttonVariant: 
         {
-        primary: "bg-primary text-on-primary shadow-primary box-shadow-on-primary",
-        secondary: "bg-secondary text-on-secondary shadow-(--box-shadow-on-primary)",
-        "secondary-reversed": "bg-on-secondary text-secondary shadow-(--box-shadow-on-primary)",
-        "on-secondary": "bg-primary text-on-secondary shadow-(--box-shadow-on-secondary)",
-        "simple-link": "bg-transparent text-on-tertiary",
+        secondary: "bg-secondary text-on-secondary shadow-(--box-shadow-on-primary) hover:shadow-(--box-shadow-tertiary)",
+        "secondary-reversed": "bg-on-secondary text-secondary shadow-(--box-shadow-on-primary) hover:shadow-(--box-shadow-tertiary)",
+        "on-secondary": "bg-primary text-on-secondary shadow-(--box-shadow-on-secondary) hover:shadow-(--box-shadow-tertiary)",
+        "simple-link": "bg-transparent text-on-tertiary hover:text-on-primary",
         }
         
 
