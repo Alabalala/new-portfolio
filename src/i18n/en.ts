c8a867f42dad0d010d381453d20d18815733266c
Alabalala <allareiksa@gmail.com>
2025-11-24 13:24:05 +0100

    feat: en translations

---
+++ src/i18n/en.ts
@@ -0,0 +1,43 @@
+export default {
+  about: {
+    title: "ABOUT ME",
+    p1: "I’m a bilingual junior web developer with 1 year of experience.",
+    p2: "I love building clean, user-friendly interfaces that balance design and functionality. I always begin in Figma, where I prototype and experiment before turning my ideas into websites coded in React, Next.js, and Astro.",
+    p3: "Everything you see on this page, including the illustrations, was designed by me in Figma.",
+  },
+
+  footer: {
+    madeBy: "Created with ❤️ by Álvaro Alarcón",
+    backToTop: "Back to top",
+  },
+
+  intro: {
+    title: "LET'S NOT WASTE TIME",
+    subtitle: "If you’re looking for a web dev who knows...",
+    punchline: "I'M YOUR GUY!",
+    buttons: {
+      learnMore: "LEARN MORE",
+      contactMe: "CONTACT ME",
+    },
+  },
+
+  projects: {
+    title: "PROJECTS",
+    visitWebsite: "VISIT WEBSITE",
+  },
+
+  contact: {
+    title: "CONTACT",
+    form: {
+      name: "Your name",
+      placeholderName: "E. g. Álvaro Alarcón",
+      email: "Your email",
+      placeholderEmail: "E. g. name@domain.com",
+      message: "Your message",
+      placeholderMessage: "Write here almost as much as you want",
+      CTA: "Send",
+      autoresponse:
+        "Hi! Thanks for reaching out. I'll take a look at your message as soon as possible. Have a nice day! Álvaro",
+    },
+  },
+};
