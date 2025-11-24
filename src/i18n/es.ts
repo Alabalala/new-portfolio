5d669662550a54ab5fc957bca55ef02cc914e6e9
Alabalala <allareiksa@gmail.com>
2025-11-24 13:24:15 +0100

    feat: es translations

---
+++ src/i18n/es.ts
@@ -0,0 +1,43 @@
+export default {
+  about: {
+    title: "SOBRE MÍ",
+    p1: "Soy un desarrollador web junior bilingüe con 1 año de experiencia.",
+    p2: "Me encanta crear interfaces limpias, intuitivas y equilibradas entre diseño y funcionalidad. Siempre empiezo en Figma, donde prototipo y experimento antes de convertir mis ideas en páginas web creadas con React, Next.js y Astro.",
+    p3: "Todo lo que ves en esta página, incluidas las ilustraciones, lo he diseñado yo mismo con Figma.",
+  },
+
+  footer: {
+    madeBy: "Creado con ❤️ por Álvaro Alarcón",
+    backToTop: "Volver arriba",
+  },
+
+  intro: {
+    title: "NO VAMOS A PERDER EL TIEMPO",
+    subtitle: "Si buscas un desarrollador web que sabe...",
+    punchline: "¡ESE SOY YO!",
+    buttons: {
+      learnMore: "MÁS INFO",
+      contactMe: "CONTÁCTAME",
+    },
+  },
+
+  projects: {
+    title: "PROYECTOS",
+    visitWebsite: "VISITAR WEB",
+  },
+
+  contact: {
+    title: "CONTACTO",
+    form: {
+      name: "Tu nombre",
+      placeholderName: "P. ej. Álvaro Alarcón",
+      email: "Tu email",
+      placeholderEmail: "P. ej. nombre@dominio.com",
+      message: "Tu mensaje",
+      placeholderMessage: "Escribe aquí todo lo que quieras",
+      CTA: "Enviar",
+      autoresponse:
+        "¡Hola! Gracias por ponerte en contacto. Revisaré tu mensaje lo antes posible. ¡Que tengas un buen día! Álvaro",
+    },
+  },
+};
