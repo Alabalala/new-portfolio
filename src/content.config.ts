a4ef6283d766d19cd1d1c3f7795df0a16a79b1d4
Alabalala <allareiksa@gmail.com>
2026-07-15 12:10:58 +0200

    Chore: Update .slug to .id and entry.render() to render(entry)

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,24 +1,28 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
+import { glob } from "astro/loaders";
 
 export const collections = {
   projects: defineCollection({
+    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/projects" }),
     schema: z.object({
       title: z.string(),
       image: z.string(),
       color: z.string().optional(),
       tags: z.array(z.string()).optional(),
-      link: z.string().url().optional(),
+      link: z.url().optional(),
       summary: z.string().optional(),
       blog: z.boolean().optional(),
-      date: z.date(),
+      date: z.coerce.date(),
       locale: z.string(),
     }),
   }),
   blogs: defineCollection({
+    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blogs" }),
     schema: z.object({
       title: z.string(),
       summary: z.string(),
-      date: z.date(),
+      date: z.coerce.date(),
       cover: z.string().optional(),
       locale: z.string().optional(),
     }),
